5a9643be3b221e662185347bb5765ce4b33498a2
veldakarimi <veldakarimi@gmail.com>
2023-08-12 06:15:06 +0300

    added POST method and resource

--- APIFestSwag/swags/urls.py
+++ APIFestSwag/swags/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from swags.views import SwagListView, SwagDetailView
+from swags.views import SwagListView, SwagDetailView, SwagCreateView
 
 app_name='swags'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('swag/', SwagListView.as_view(), name='swag-list'),
     path('swag/<uuid:pk>/', SwagDetailView.as_view(), name='swag-detail'),
+    path('create_swag/', SwagCreateView.as_view(), name='create-swag'),
 ]
--- APIFestSwag/swags/views.py
+++ APIFestSwag/swags/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from swags.models import Swag
 from swags.serializers import SwagSerializer
 
@@ -8,5 +9,9 @@ class SwagListView(ListAPIView):
     serializer_class = SwagSerializer
 
 class SwagDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Swag.objects.all()
+    serializer_class = SwagSerializer
+
+class SwagCreateView(generics.CreateAPIView):
     queryset = Swag.objects.all()
     serializer_class = SwagSerializer
